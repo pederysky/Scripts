8fa82e8308094180661def66d588a4ba6268defa
pederysky <145128880+pederysky@users.noreply.github.com>
2025-04-07 13:24:35 +0200

    Update reinicio.py

--- Python/reinicio.py
+++ Python/reinicio.py
@@ -29,4 +29,4 @@ def reiniciar_servidor():
         return jsonify({"error": f"Error al reiniciar Tomcat: {e}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8081)
